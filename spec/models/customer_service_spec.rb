0d3f2a82b758024f3d3f11781eb546b80a61c86b
klauslube <klauslube93@gmail.com>
2023-12-14 18:44:33 -0300

    0003-[Refac]: identation

--- spec/models/customer_service_spec.rb
+++ spec/models/customer_service_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-describe CustomerService do 
+describe CustomerService do
   context 'validations' do
     it { should validate_presence_of(:code) }
     it { should validate_presence_of(:service_type) }
     it { should validate_presence_of(:medical_service_type) }
     # TODO: it { should belong_to(:customer) }
     it do
-      should define_enum_for(:service_type).
-      with_values(default: 5, preferential: 10)
+      should define_enum_for(:service_type)
+        .with_values(default: 5, preferential: 10)
     end
-    
+
     it do
-      should define_enum_for(:medical_service_type).
-      with_values(general: 5, specialist: 10)
+      should define_enum_for(:medical_service_type)
+        .with_values(general: 5, specialist: 10)
     end
   end
 end
